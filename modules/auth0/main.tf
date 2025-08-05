4e8dd739a1758c3aa39b8040016c4323207a34ad
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-06 05:28:57 +0530

    Update main.tf

--- modules/auth0/main.tf
+++ modules/auth0/main.tf
@@ -1,3 +1,13 @@
+
+terraform {
+  required_providers {
+    auth0 = {
+      source  = "auth0/auth0"
+      version = "~> 1.0"
+    }
+  }
+}
+
 provider "auth0" {
   domain        = var.auth0_domain
   client_id     = var.auth0_provider_client_id
