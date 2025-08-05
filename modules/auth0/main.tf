d4a2f1b5803674ca2066ea02da8600c2317df104
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-06 04:13:42 +0530

    Create main.tf

---
+++ modules/auth0/main.tf
@@ -0,0 +1,25 @@
+provider "auth0" {
+  domain        = var.auth0_domain
+  client_id     = var.auth0_provider_client_id
+  client_secret = var.auth0_provider_client_secret
+}
+
+resource "auth0_client" "hello_world_app" {
+  name            = "HelloWorldApp"
+  app_type        = "regular_web"
+  oidc_conformant = true
+
+  callbacks = [
+    "https://${var.cloudfront_domain_name}/index.html"
+  ]
+
+  allowed_logout_urls = [
+    "https://${var.cloudfront_domain_name}/logout"
+  ]
+
+  grant_types = [
+    "authorization_code",
+    "implicit",
+    "refresh_token"
+  ]
+}
