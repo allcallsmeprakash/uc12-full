65a26fdb3c3c1844509185faec63e6b3f81caa10
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-06 04:14:32 +0530

    Create outputs.tf

---
+++ modules/auth0/outputs.tf
@@ -0,0 +1,9 @@
+output "auth0_client_id" {
+  description = "Auth0 client ID"
+  value       = auth0_client.hello_world_app.client_id
+}
+
+output "auth0_client_secret" {
+  description = "Auth0 client secret"
+  value       = auth0_client.hello_world_app.client_secret
+}
