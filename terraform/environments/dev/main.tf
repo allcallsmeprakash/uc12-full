636b85c29fc9ed9ca36d10677b875a1738f41280
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-06 05:24:16 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -14,13 +14,13 @@ terraform {
 
 
 module "s3_cloudfront" {
-  source = "../../modules/s3_cloudfront"
+  source = "../../../modules/s3_cloudfront"
   s3_bucket_name = var.s3_bucket_name
   cloudfront_default_root_object = var.cloudfront_default_root_object
 }
 
 module "auth0" {
-  source = "../../modules/auth0"
+  source = "../../../modules/auth0"
   auth0_domain = var.auth0_domain
   auth0_provider_client_id = var.auth0_provider_client_id
   auth0_provider_client_secret = var.auth0_provider_client_secret
@@ -28,7 +28,7 @@ module "auth0" {
 }
 
 module "aws_cognito" {
-  source = "../../modules/aws_cognito"
+  source = "../../../modules/aws_cognito"
   auth0_client_id = module.auth0.auth0_client_id
   auth0_client_secret = module.auth0.auth0_client_secret
   auth0_domain = var.auth0_domain
@@ -36,7 +36,7 @@ module "aws_cognito" {
 }
 
 module "lambda" {
-  source = "../../modules/lambda"
+  source = "../../../modules/lambda"
   lambda_function_name = var.lambda_function_name
   lambda_handler = var.lambda_handler
   lambda_runtime = var.lambda_runtime
@@ -44,7 +44,7 @@ module "lambda" {
 }
 
 module "api_gateway" {
-  source = "../../modules/api_gateway"
+  source = "../../../modules/api_gateway"
   lambda_invoke_arn = module.lambda.lambda_invoke_arn
   cognito_user_pool_arn = module.aws_cognito.user_pool_arn
 }
