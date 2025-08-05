2f032d81d06f116cc6a1987e1a2fd313f292646f
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-06 04:18:36 +0530

    Create main.tf

---
+++ modules/api_gateway/main.tf
@@ -0,0 +1,54 @@
+resource "aws_api_gateway_rest_api" "hello_api" {
+  name        = "hello-world-api"
+  description = "API for Hello World Lambda"
+}
+
+resource "aws_api_gateway_resource" "hello_resource" {
+  rest_api_id = aws_api_gateway_rest_api.hello_api.id
+  parent_id   = aws_api_gateway_rest_api.hello_api.root_resource_id
+  path_part   = "hello"
+}
+
+resource "aws_api_gateway_authorizer" "cognito_auth" {
+  name            = "CognitoAuthorizer"
+  rest_api_id     = aws_api_gateway_rest_api.hello_api.id
+  identity_source = "method.request.header.Authorization"
+  type            = "COGNITO_USER_POOLS"
+  provider_arns   = [var.cognito_user_pool_arn]
+}
+
+resource "aws_api_gateway_method" "hello_method" {
+  rest_api_id   = aws_api_gateway_rest_api.hello_api.id
+  resource_id   = aws_api_gateway_resource.hello_resource.id
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.cognito_auth.id
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.hello_api.id
+  resource_id             = aws_api_gateway_resource.hello_resource.id
+  http_method             = aws_api_gateway_method.hello_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.lambda_invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "hello_deployment" {
+  depends_on  = [aws_api_gateway_integration.lambda_integration]
+  rest_api_id = aws_api_gateway_rest_api.hello_api.id
+}
+
+resource "aws_api_gateway_stage" "api_stage" {
+  stage_name    = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.hello_api.id
+  deployment_id = aws_api_gateway_deployment.hello_deployment.id
+
+  variables = {
+    lambdaAlias = "prod"
+  }
+
+  tags = {
+    Environment = "prod"
+  }
+}
