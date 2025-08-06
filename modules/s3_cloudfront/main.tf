d0acb9af1b4ddcd043cfece0dc95696f75350714
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-06 12:00:08 +0530

    Update main.tf

--- modules/s3_cloudfront/main.tf
+++ modules/s3_cloudfront/main.tf
@@ -43,17 +43,35 @@ resource "aws_cloudfront_distribution" "website_distribution" {
   origin {
     domain_name = aws_s3_bucket_website_configuration.website_config.website_endpoint
     origin_id   = "S3Origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
   }
 
   enabled             = true
   is_ipv6_enabled     = true
-  default_root_object = var.cloudfront_default_root_object
+  default_root_object = "index.html"
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "S3Origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
     viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 
   restrictions {
