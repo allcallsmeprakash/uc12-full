7ce4d5c55667ba6fa4d9acae529ff765c45261a3
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-06 14:23:43 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -34,3 +34,76 @@ resource "aws_lambda_function" "hello_world" {
   source_code_hash = filebase64sha256(data.archive_file.lambda_zip.output_path)
   depends_on       = [aws_iam_role.lambda_exec_role]
 }
+
+
+resource "aws_cloudwatch_log_group" "api_gateway" {
+  name              = "/aws/apigateway/lambda-http-api"
+  retention_in_days = 7
+}
+
+resource "aws_cloudwatch_log_group" "lambda" {
+  name              = "/aws/lambda/hello-world-lambda"
+  retention_in_days = 7
+}
+
+resource "aws_cloudwatch_dashboard" "main" {
+  dashboard_name = "Lambda-dashboard"
+  dashboard_body = jsonencode({
+    widgets = [
+      {
+        type   = "metric"
+        x      = 0
+        y      = 0
+        width  = 12
+        height = 6
+        properties = {
+          metrics = [
+            [
+              "AWS/ApiGateway",
+              "4XXError",
+              "ApiName",
+              "lambda-http-api"
+            ],
+            [
+              "AWS/ApiGateway",
+              "5XXError",
+              "ApiName",
+              "lambda-http-api"
+            ]
+          ]
+          period = 300
+          stat   = "Sum"
+          region = "us-east-1"
+          title  = "API Gateway Errors"
+        }
+      },
+      {
+        type   = "metric"
+        x      = 12
+        y      = 0
+        width  = 12
+        height = 6
+        properties = {
+          metrics = [
+            [
+              "AWS/Lambda",
+              "Errors",
+              "FunctionName",
+              "hello-world-lambda"
+            ],
+            [
+              "AWS/Lambda",
+              "Throttles",
+              "FunctionName",
+              "hello-world-lambda"
+            ]
+          ]
+          period = 300
+          stat   = "Sum"
+          region = "us-east-1"
+          title  = "Lambda Errors/Throttles"
+        }
+      }
+    ]
+  })
+}
