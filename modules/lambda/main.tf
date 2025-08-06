fcaea1bb349f8f094d90d002db18808e2e9f2bc9
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-06 11:51:34 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -27,6 +27,7 @@ data "archive_file" "lambda_zip" {
 
 resource "aws_lambda_function" "hello_world" {
   function_name    = var.lambda_function_name
+  filename         = data.archive_file.lambda_zip.output_path
   role             = aws_iam_role.lambda_exec_role.arn
   handler          = var.lambda_handler
   runtime          = var.lambda_runtime
